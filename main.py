4d2303a9eebefd8d9bea717d2222ebf9dd910aa3
NetZach777 <150202716+NetZach777@users.noreply.github.com>
2024-03-17 17:10:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def load_data(filepath):
 def get_sentiment_prediction(tweet_text):
     tweet_text = preprocess_text(tweet_text)
     
-    url = "https://2ec7-2a01-cb04-ae6-c900-c88c-9df2-f21e-4346.ngrok-free.app/predict-sentiment/"
+    url = "https://9ba2-2a01-cb04-ae6-c900-d6a1-afc7-bf95-2370.ngrok-free.app/predict-sentiment/"
     payload = {"text": tweet_text}
     try:
         response = requests.post(url, json=payload)
